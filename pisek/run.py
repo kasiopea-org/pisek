6b7373e19f6273aac74f05ab329ca4101e2dca64
Jiři Beneš <mail@jiribenes.com>
2019-12-20 16:04:44 +0100

    Přidat malé spouštítko pro `./exec < in > out`

---
+++ pisek/run.py
@@ -0,0 +1,17 @@
+import subprocess
+
+# TODO: Adapt the code from https://gist.github.com/s3rvac/f97d6cbdfdb15c0a32e7e941f7f4a3fa
+#       to limit the memory of the subprocess
+
+
+def run(executable: str, input_file: str, output_file: str, timeout: int = 100) -> None:
+    with open(input_file, "r") as inp:
+        with open(output_file, "w") as outp:
+            result = subprocess.run(
+                [f"./{executable}"],
+                stdin=inp,
+                stdout=outp,
+                stderr=subprocess.PIPE,
+                timeout=timeout,
+            )
+            result.check_returncode()  # raises an exception if return code != 0
