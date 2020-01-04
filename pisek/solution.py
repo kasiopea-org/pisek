fff50297cb381b863691b3fafb91802d2dbe12d0
Václav Volhejn <vaclav.volhejn@gmail.com>
2020-01-05 00:11:40 +0100

    Spojit run.py a program.py

--- pisek/solution.py
+++ pisek/solution.py
@@ -1,10 +1,9 @@
 import os
 
-from .program import Program
-from . import run
+from . import program
 
 
-class Solution(Program):
+class Solution(program.Program):
     def run_on_file(self, input_file):
         """ Runs the solution and stores the output in a reasonably named file in data/ """
         data_dir = os.path.join(self.task_dir, "data/")
@@ -19,5 +18,5 @@ class Solution(Program):
         self.compile_if_needed()
 
         # self.assertTrue(f"Chyba při spuštění {self.name} na {input_file}",)
-        ok = run.run(self.executable, input_file, output_file)
+        ok = program.run(self.executable, input_file, output_file)
         return output_file if ok else None
