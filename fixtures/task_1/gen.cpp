ccb0739f5a9b42cfaf81efefa6967f8f36f2235f
Václav Volhejn <vaclav.volhejn@gmail.com>
2020-01-06 22:43:27 +0100

    Přidat skutečný generátor

--- fixtures/task_1/gen.cpp
+++ fixtures/task_1/gen.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <string>
+#include <random>
+using ll = long long;
+using namespace std;
+
+std::mt19937_64 rng;
+
+ll randRange(ll from, ll to) {
+    std::uniform_int_distribution<ll> dist(from, to);
+    return dist(rng);
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         return 1;
     }
 
-    int diff = std::atoi(argv[1]);
-    int seed = std::strtoull(argv[2], NULL, 16) & 0x7fffffff;
+    int diff = atoi(argv[1]) - 1;
+    int seed = strtoull(argv[2], NULL, 16) & 0x7fffffff;
+    rng.seed(seed);
 
-    // print out the difficulty
-    std::cout << diff << std::endl;
+    ll MAX_ABS = (diff == 1) ? 1e18 : 1e9;
 
-    // print out the seed
-    std::cout << seed << std::endl;
+    int T = 10;
+    cout << T << endl;
+
+    for (int ti = 0; ti < T; ti++) {
+        ll a = randRange(-MAX_ABS, MAX_ABS);
+        ll b = randRange(-MAX_ABS, MAX_ABS);
+        cout << a << " " << b << endl;
+    }
 
     return 0;
 }
