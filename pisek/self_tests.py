a1b699bbbeedfaef973f6f7537d7313b70053202
Václav Volhejn <IAmWave@users.noreply.github.com>
2020-01-24 22:14:43 +0100

    Opravit chování diffů při neexistujících souborech (#25)
    Také lepší chybové hlášky při selhání self-testů

--- pisek/self_tests.py
+++ pisek/self_tests.py
@@ -7,6 +7,7 @@ import unittest
 import shutil
 import tempfile
 import os
+import io
 
 import pisek.tests
 
@@ -38,18 +39,23 @@ class TestTask1(unittest.TestCase):
         # and we want to consider it a timeout
         suite = pisek.tests.kasiopea_test_suite(self.task_dir, timeout=1)
 
-        with open(os.devnull, "w") as devnull:
-            runner = unittest.TextTestRunner(stream=devnull, failfast=True)
+        # with open(os.devnull, "w") as devnull:
+        output = io.StringIO()
+        runner = unittest.TextTestRunner(stream=output, failfast=True)
 
-            result = runner.run(suite)
+        result = runner.run(suite)
+
+        out = output.getvalue()
+        out = "\n".join([f"> {x}" for x in out.split("\n")])
 
         self.assertEqual(
             result.wasSuccessful(),
             self.expecting_success(),
-            "Neočekávaný výsledek testu: test {}měl projít, ale {}prošel".format(
+            "Neočekávaný výsledek testu: test {}měl projít, ale {}prošel.".format(
                 "" if self.expecting_success() else "ne",
                 "" if result.wasSuccessful() else "ne",
-            ),
+            )
+            + " Výstup testu:\n{}".format(out),
         )
 
     def tearDown(self):
