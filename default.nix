0ea3d0ee676ef9ee44f7f2b7d036fa5602249630
Jiří Beneš <mail@jiribenes.com>
2020-09-19 20:00:03 +0200

    Přidat podporu pro Nix a Poetry

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ sources ? import ./nix/sources.nix, nixpkgs ? sources.nixpkgs }:
+
+let
+  pkgs = import nixpkgs {};
+
+  pisek-exe = pkgs.poetry2nix.mkPoetryApplication {
+    projectDir = ./.;
+  };
+
+  pisek-env = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    #editablePackageSources = {
+    #  pisek = ./.;
+    #};
+  };
+
+  shell = pkgs.mkShell {
+    buildInputs = with pkgs; [
+      niv
+      nixpkgs-fmt
+
+      poetry
+      pisek-env
+      mypy
+      python3Packages.black
+
+      dhall
+      dhall-json
+    ];
+  };
+in
+{
+  inherit pisek-exe pisek-env shell;
+
+}
