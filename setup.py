9526bd398cd3729fc4a2fc15f93cc022b588516c
Václav Volhejn <vaclav.volhejn@gmail.com>
2021-03-31 21:59:46 +0200

    Přidat termcolor do setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.1",
     description="Nástroj na kontrolování úloh",
     packages=setuptools.find_packages(),
-    install_requires=["tqdm>=4.50"],
+    install_requires=["tqdm>=4.50", "termcolor>=1.1"],
     extras_require={"dev": ["black", "mypy"]},
     entry_points={"console_scripts": ["pisek=pisek.__main__:main_wrapped"]},
 )
