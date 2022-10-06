7e5caf3fbe9145ec778ec9d64421d6514d989107
Jan Kaifer <jan@kaifer.cz>
2022-10-06 14:51:47 +0200

    Add nix stuff

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ sources ? import ./nix/sources.nix, nixpkgs ? sources.nixpkgs }:
+
+let
+  pkgs = import nixpkgs {};
+
+  pisek-exe = pkgs.poetry2nix.mkPoetryApplication {
+    projectDir = ./.;
+  };
+
+  pisek-env = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    #editablePackageSources = {
+    #  pisek = ./.;
+    #};
+  };
+
+  # vývojový shell
+  shell = pkgs.mkShell {
+    buildInputs = with pkgs; [
+      niv
+      nixpkgs-fmt
+
+      poetry
+      pisek-env
+      mypy
+      python3Packages.black
+    ];
+  };
+in
+{
+  inherit pisek-exe pisek-env shell;
+
+}
