8c84836da1991812828fa81ffefc5d3e3aa85428
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2022-12-04 14:23:47 +0100

    Lepší formát částečných bodů

--- pisek/tests/test_case.py
+++ pisek/tests/test_case.py
@@ -228,7 +228,7 @@ class SolutionWorks(SolutionTestCase):
             )
 
             if run_result.kind == RunResultKind.OK:
-                c = "·" if pts == 1 else "W" if pts == 0 else f"[{pts:.2}]"
+                c = "·" if pts == 1 else "W" if pts == 0 else f"[{pts:.2f}]"
 
                 if pts != 1 and run_result.msg:
                     messages.append(run_result.msg)
