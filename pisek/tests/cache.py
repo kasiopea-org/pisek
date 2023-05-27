a231e32f0b4f7d8cc5f8a40cec6048f0da1bc571
Daniel Skýpala <daniel@honza.info>
2023-05-27 12:43:41 +0200

    Added Cache source code

---
+++ pisek/tests/cache.py
@@ -0,0 +1,42 @@
+from typing import List, Dict, Any
+import os
+import yaml
+
+CACHE_FILENAME = ".pisek_cache"
+SAVED_LAST_SIGNATURES = 5
+
+class CacheEntry(yaml.YAMLObject):
+    yaml_tag = u'!Entry'
+    def __init__(self, name: str, signature: str, result: str, envs: Dict[str, str], files: List[str]) -> None:
+        self.name = name
+        self.signature = signature
+        self.result = result
+        self.envs = envs
+        self.files = files
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name}, signature={self.signature}" \
+               f"result={self.result}, envs={self.envs}, files={self.files})"
+
+    def yaml_export(self) -> str:
+        return yaml.dump([self])
+
+class Cache:
+    def __init__(self, env) -> None:
+        self.cache_path = os.path.join(env.task_dir, CACHE_FILENAME)
+        self.cache = self._load(self.cache_path)
+
+    def add(self, name: str, result: str):
+        self.cache[name] = CacheEntry(name, result)
+        with open(self.cache_path, 'a') as f:
+            f.write(self.cache[name].yaml_export())
+
+    def _load(self) -> Dict[str, CacheEntry]:
+        if not os.path.exists(self.cache_path):
+            return {}
+
+        cache = {}
+        with open(self.cache_path) as f:
+            for entry in yaml.full_load(f):
+                cache[entry.name] = entry
+        return cache
