0652b18de4ae749d7011f732ed2b6a11fec55ad5
Daniel Skýpala <daniel@honza.info>
2023-06-15 17:11:08 +0200

    __init__

---
+++ pisek/jobs/parts/__init__.py
@@ -0,0 +1,20 @@
+# pisek  - Nástroj na přípravu úloh do programátorských soutěží, primárně pro soutěž Kasiopea.
+#
+# Copyright (c)   2019 - 2022 Václav Volhejn <vaclav.volhejn@gmail.com>
+# Copyright (c)   2019 - 2022 Jiří Beneš <mail@jiribenes.com>
+# Copyright (c)   2020 - 2022 Michal Töpfer <michal.topfer@gmail.com>
+# Copyright (c)   2022        Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from .samples import SampleManager
+from .generator import GeneratorManager
+from .checker import CheckerManager
+from .judge import JudgeManager
+from .solution import SolutionManager
