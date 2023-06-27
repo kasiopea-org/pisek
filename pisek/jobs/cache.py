bf44cdbd355bedc4b257f1d13480e46554f1768e
Daniel Skýpala <daniel@honza.info>
2023-06-27 22:03:03 +0200

    Cache: comments

--- pisek/jobs/cache.py
+++ pisek/jobs/cache.py
@@ -7,6 +7,7 @@ CACHE_FILENAME = ".pisek_cache"
 SAVED_LAST_SIGNATURES = 5
 
 class CacheResultEnum:
+    """Enum of strings."""
     def __init__(self, *values):
         self.values = values
         for value in values:
@@ -41,6 +42,7 @@ class Cache:
         self.cache = self._load()
 
     def add(self, cache_entry: CacheEntry):
+        """Add entry to cache."""
         self.cache[cache_entry.name] = cache_entry
         with open(self.cache_path, 'a', encoding='utf-8') as f:
             f.write(cache_entry.yaml_export())
@@ -52,6 +54,7 @@ class Cache:
         return self.cache[name]
 
     def _load(self) -> dict[str, CacheEntry]:
+        """Load cache file."""
         if not os.path.exists(self.cache_path):
             return {}
 
@@ -66,6 +69,7 @@ class Cache:
         return cache
 
     def export(self) -> None:
+        """Export cache into a file."""
         with open(self.cache_path, 'w', encoding='utf-8') as f:
             for cache_entry in self.cache.values():
                 f.write(cache_entry.yaml_export())
