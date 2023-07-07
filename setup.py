1ff1a770480b3c679287505d037d932c8487f7f2
Daniel Skýpala <daniel@honza.info>
2023-07-07 20:27:39 +0200

    Remove redundant libary

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     version="0.1",
     description="Nástroj na kontrolování úloh",
     packages=setuptools.find_packages(),
-    install_requires=["tqdm>=4.50", "termcolor>=1.1", "types-termcolor"],
+    install_requires=["termcolor>=1.1", "types-termcolor"],
     extras_require={"dev": ["black", "mypy"]},
     entry_points={"console_scripts": ["pisek=pisek.__main__:main_wrapped"]},
 )
