db47580bf814188bafbf286b6c33262e871c0a51
Benjamin Swart <Benjaminswart@email.cz>
2024-02-05 20:59:14 +0000

    CMS: Add uploading testcases

--- pisek/cms/__init__.py
+++ pisek/cms/__init__.py
@@ -2,14 +2,25 @@ from cms.db.session import Session
 
 from pisek.cms.task import create_task
 from pisek.task_config import TaskConfig
+from pisek.jobs.task_pipeline import TaskPipeline
+from pisek.pipeline_tools import PATH, run_pipeline
+
+
+def prepare_files(config):
+    contest_type = config.contest_type
+
+    if contest_type != "cms":
+        raise RuntimeError(f"Cannot upload {contest_type}-type task to CMS")
+
+    run_pipeline(PATH, TaskPipeline, solutions=[config.primary_solution])
 
 
 def create(args):
     config = TaskConfig(".")
+    prepare_files(config)
+
     session = Session()
 
-    name = config["task_name"]
-
-    create_task(session, name)
+    create_task(session, config)
 
     session.commit()
--- pisek/cms/task.py
+++ pisek/cms/task.py
@@ -1,20 +1,16 @@
-from cms.db.task import Task, Dataset
+from cms.db.task import Task
 from sqlalchemy.orm import Session
 import re
 
+from pisek.cms.dataset import create_dataset
+from pisek.task_config import TaskConfig
 
-def create_task(session: Session, name: str):
+
+def create_task(session: Session, config: TaskConfig):
+    name = config.task_name
+
     task = Task(name=name, title=name, submission_format=[get_default_file_name(name)])
-
-    dataset = Dataset(
-        description="Default",
-        autojudge=True,
-        task_type="Batch",
-        task_type_parameters=["alone", ["", ""], "diff"],
-        score_type="Sum",
-        score_type_parameters=100,
-        task=task,
-    )
+    dataset = create_dataset(session, config, task)
 
     task.active_dataset = dataset
 
