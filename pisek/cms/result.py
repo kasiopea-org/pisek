3a7f1c998833785cff0d5f382f6b55fd756102bf
Benjamin Swart <Benjaminswart@email.cz>
2024-02-17 14:04:28 +0000

    CMS: Check subtask scores

--- pisek/cms/result.py
+++ pisek/cms/result.py
@@ -8,7 +8,7 @@ import json
 
 from pisek.cms.submission import get_submission
 from pisek.jobs.parts.testing_log import TESTING_LOG
-from pisek.task_config import SolutionConfig, TaskConfig
+from pisek.task_config import SolutionConfig, TaskConfig, SubtaskConfig
 
 
 def create_testing_log(session: Session, config: TaskConfig, dataset: Dataset):
@@ -62,6 +62,7 @@ def create_testing_log(session: Session, config: TaskConfig, dataset: Dataset):
 def check_results(session: Session, config: TaskConfig, dataset: Dataset):
     files = FileCacher()
 
+    solution: SolutionConfig
     for name, solution in config.solutions.subenvs():
         try:
             result = get_submission_result(session, files, config, solution, dataset)
@@ -89,6 +90,63 @@ def check_results(session: Session, config: TaskConfig, dataset: Dataset):
             )
         else:
             print(f"{name}: {score} points")
+
+        subtasks: list[tuple[str, SubtaskConfig]] = list(config.subtasks.subenvs())
+        fractions = get_subtask_score_fractions(result.score_details)
+
+        if fractions is None or len(fractions) != len(subtasks):
+            print(
+                f"  {Fore.YELLOW}The task seems to use an unsupported score type, skipping checking subtasks{Fore.RESET}"
+            )
+            continue
+
+        target: str
+        for (num, subtask), fraction, target in zip(
+            subtasks, fractions, solution.subtasks
+        ):
+            name = subtask.name or f"Subtask {num}"
+
+            if target == "X":
+                correct = True
+            elif target == "1":
+                target_name = "correct"
+                correct = fraction == 1.0
+            elif target == "P":
+                target_name = "partially correct"
+                correct = 0.0 < fraction < 1.0
+            else:
+                target_name = "wrong"
+                correct = fraction == 0.0
+
+            if correct:
+                print(f"  {name}: {fraction}")
+            else:
+                print(
+                    f"  {Fore.RED}{name}: {fraction} (should be {target_name}){Fore.RESET}"
+                )
+
+
+def get_subtask_score_fractions(score_details: Any) -> Optional[list[float]]:
+    if not isinstance(score_details, list):
+        return None
+
+    results = []
+
+    for subtask in score_details:
+        if not isinstance(subtask, dict):
+            return None
+
+        if "score_fraction" not in subtask:
+            return None
+
+        fraction = subtask["score_fraction"]
+
+        if not isinstance(fraction, float):
+            return None
+
+        results.append(fraction)
+
+    return results
 
 
 def get_submission_result(
