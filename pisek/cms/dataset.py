97f92fcc9080f7234c9957d0030d3ce39d2f6e9d
Benjamin Swart <Benjaminswart@email.cz>
2024-02-28 19:02:13 +0000

    CMS: Use CMS config for importing

--- pisek/cms/dataset.py
+++ pisek/cms/dataset.py
@@ -50,12 +50,8 @@ def create_dataset(
         task_type_parameters=task_params,
         score_type="GroupMin",
         score_type_parameters=score_params,
-        time_limit=config.limits.solve.time_limit,
-        memory_limit=(
-            config.limits.solve.mem_limit * 1024 * 1024
-            if config.limits.solve.mem_limit > 0
-            else None
-        ),
+        time_limit=config.cms.time_limit,
+        memory_limit=config.cms.mem_limit * 1024 * 1024,
         task=task,
     )
 
--- pisek/cms/task.py
+++ pisek/cms/task.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from cms.db.task import Task
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
@@ -10,15 +11,32 @@ from pisek.env.task_config import TaskConfig
 
 def create_task(session: Session, env: Env, description: str) -> Task:
     config = env.config
-    name = config.name
 
-    task = Task(name=name, title=name, submission_format=[get_default_file_name(name)])
+    task = Task(name=config.name, title=config.name)
+    set_task_settings(task, config)
+
     dataset = create_dataset(session, env, task, description)
 
     task.active_dataset = dataset
 
     session.add(task)
     return task
+
+
+def set_task_settings(task: Task, config: TaskConfig):
+    task.title = config.cms.title or config.name
+    task.submission_format = config.cms.submission_format or [
+        get_default_file_name(config.name)
+    ]
+    task.max_submission_number = config.cms.max_submissions
+    task.min_submission_interval = (
+        timedelta(seconds=config.cms.min_submission_interval)
+        if config.cms.min_submission_interval is not None
+        else None
+    )
+    task.score_precision = config.cms.score_precision
+    task.score_mode = config.cms.score_mode
+    task.feedback_level = config.cms.feedback_level
 
 
 def get_task(session: Session, config: TaskConfig):
