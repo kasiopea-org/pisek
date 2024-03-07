c79043f06073ca5318655f2ab864c61b9454af58
Daniel Skýpala <daniel@honza.info>
2024-03-07 14:22:13 +0100

    VerdictsEval: Add licence

--- pisek/jobs/parts/verdicts_eval.py
+++ pisek/jobs/parts/verdicts_eval.py
@@ -1,3 +1,16 @@
+# pisek  - Tool for developing tasks for programming competitions.
+#
+# Copyright (c)   2023        Daniel Skýpala <daniel@honza.info>
+# Copyright (c)   2024        Benjamin Swart <Benjaminswart@email.cz>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from typing import Optional
 
 from pisek.env.task_config import TaskConfig, FailMode
