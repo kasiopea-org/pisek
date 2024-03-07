a85e71a30f9c3731b1a5a3a2ec0dfd8e09374931
Daniel Skýpala <54771191+5K1PY@users.noreply.github.com>
2024-03-07 14:07:03 +0100

    VerdictsEval: Split evaluation of any / all
    Co-authored-by: Benjamin Swart <32597377+SvizelPritula@users.noreply.github.com>

--- pisek/jobs/parts/verdicts_eval.py
+++ pisek/jobs/parts/verdicts_eval.py
@@ -7,24 +7,28 @@ from pisek.jobs.parts.solution_result import Verdict, SUBTASK_SPEC, verdict_alwa
 def evaluate_verdicts(
     config: TaskConfig, verdicts: list[Verdict], expected: str
 ) -> tuple[bool, bool, Optional[int]]:
-    mode_quantifier = all if config.fail_mode == FailMode.all else any
-
     result = True
     definitive = True
     breaker = None
-    quantifiers = [all, mode_quantifier]
-    for i, quant in enumerate(quantifiers):
-        oks = list(map(SUBTASK_SPEC[expected][i], verdicts))
-        ok = quant(oks)
 
-        result &= ok
-        definitive &= (
-            (quant == any and ok)
-            or (quant == all and not ok)
-            or (SUBTASK_SPEC[expected][i] == verdict_always)
-        )
-        if quant == all and ok == False:
-            breaker = oks.index(False)
-            break
+    modes = [FailMode.all, config.fail_mode]
+
+    for i, mode in enumerate(modes):
+        oks = list(map(SUBTASK_SPEC[expected][i], verdicts))
+
+        if mode == FailMode.all:
+            ok = all(oks)
+
+            result &= ok
+            definitive &= not ok or SUBTASK_SPEC[expected][i] == verdict_always
+
+            if not ok:
+                breaker = oks.index(False)
+                break
+        elif mode == FailMode.any:
+            ok = any(oks)
+
+            result &= ok
+            definitive &= ok
 
     return result, definitive, breaker
