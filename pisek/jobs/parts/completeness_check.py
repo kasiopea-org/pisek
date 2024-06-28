eb8ee62fbd516ab0fcffb5e17ed40744c18d5d99
Daniel Skýpala <daniel@honza.info>
2024-06-28 17:33:15 +0200

    CompletenessCheck: Add no redundant lines in CMS judge check

--- pisek/jobs/parts/completeness_check.py
+++ pisek/jobs/parts/completeness_check.py
@@ -10,6 +10,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from pisek.utils.paths import TaskPath
+from pisek.config.config_types import JudgeType
 from pisek.jobs.jobs import Job
 from pisek.jobs.parts.task_job import TaskJobManager, SOLUTION_MAN_CODE
 
@@ -22,6 +24,14 @@ class CompletenessCheck(TaskJobManager):
 
     def _get_jobs(self) -> list[Job]:
         return []
+
+    def _get_judge_outs(self) -> set[TaskPath]:
+        judge_outs = set()
+        for solution in self._env.solutions:
+            judge_outs |= self.prerequisites_results[f"{SOLUTION_MAN_CODE}{solution}"][
+                "judge_outs"
+            ]
+        return judge_outs
 
     def _check_solution_succeeds_only_on(
         self, sol_name: str, subtasks: list[int]
@@ -36,14 +46,15 @@ class CompletenessCheck(TaskJobManager):
                 return False
         return True
 
-    def _evaluate(self) -> None:
+    def _check_dedicated_solutions(self) -> None:
+        """Checks that each subtask has it's own dedicated solution."""
         if self._env.config.checks.solution_for_each_subtask:
             for num, subtask in self._env.config.subtasks.items():
                 if num == 0:
                     continue  # Samples
 
                 ok = False
-                for solution in self._env.config.solutions:
+                for solution in self._env.solutions:
                     if self._check_solution_succeeds_only_on(
                         solution, [num] + subtask.all_predecessors
                     ):
@@ -52,3 +63,16 @@ class CompletenessCheck(TaskJobManager):
 
                 if not ok:
                     self._warn(f"{subtask.name} has no dedicated solution")
+
+    def _check_cms_judge(self) -> None:
+        """Checks that cms judge's stdout & stderr contains only one line."""
+        if self._env.config.judge_type == JudgeType.cms:
+            for judge_out in self._get_judge_outs():
+                with open(judge_out.path) as f:
+                    lines = f.read().rstrip().split("\n")
+                if len(lines) > 1:
+                    return self._warn(f"{judge_out:p} contains multiple lines")
+
+    def _evaluate(self) -> None:
+        self._check_dedicated_solutions()
+        self._check_cms_judge()
