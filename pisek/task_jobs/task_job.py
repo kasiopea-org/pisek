216020db93876dd0512fb6ead7efacc090ca415f
Benjamin Swart <Benjaminswart@email.cz>
2024-11-16 10:15:57 +0100

    LinkData: Follow symlinks

--- pisek/task_jobs/task_job.py
+++ pisek/task_jobs/task_job.py
@@ -26,7 +26,7 @@ import subprocess
 from pisek.env.env import Env
 from pisek.config.task_config import ProgramLimits
 from pisek.utils.paths import TaskPath
-from pisek.utils.text import tab
+from pisek.utils.text import eprint, tab
 from pisek.config.task_config import ProgramType
 from pisek.jobs.jobs import Job
 from pisek.task_jobs.data.testcase_info import TestcaseInfo, TestcaseGenerationMode
@@ -181,7 +181,14 @@ class TaskJob(Job, TaskHelper):
         self.make_filedirs(dst)
         if overwrite and os.path.exists(dst.path):
             os.remove(dst.path)
-        return os.link(filename.path, dst.path)
+
+        # os.link should follow symlinks, but doesn't:
+        # https://bugs.python.org/issue37612
+        source = filename.path
+        while os.path.islink(source):
+            source = os.readlink(source)
+
+        return os.link(source, dst.path)
 
     @_file_access(2)
     def _files_equal(self, file_a: TaskPath, file_b: TaskPath) -> bool:
