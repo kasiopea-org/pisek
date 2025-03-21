c6ccdc3b03ad99d4d3e38b0b2382bc65c77cc7a8
Daniel Skýpala <daniel@honza.info>
2025-03-21 10:35:00 +0100

    Tests: Add test for fixtures/max

---
+++ tests/test_max.py
@@ -0,0 +1,11 @@
+import unittest
+
+from util import TestFixtureVariant
+
+
+class TestMax(TestFixtureVariant):
+    def fixture_path(self):
+        return "../fixtures/max/"
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
